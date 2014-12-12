7f8c85691298566d1d9ec3bf0aee0689d8182c49
Bob Cook <bobcook@sunrem.com>
2014-12-11 17:27:09 -0700

    seed an admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,12 +3,10 @@
 
 # Temporary admin account
 u = User.new(
-    email: "admin@example.com",
+    email: "bobcook@storyitr8.com",
     password: "1234",
     password_confirmation: "1234",
     admin: true
 )
 u.skip_confirmation!
 u.save!
-
-
